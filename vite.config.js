4538506e6cc23637436e833710d0108c95c08ef0
xn <280145668@qq.com>
2022-10-11 10:59:31 +0800

    push

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/award/',
+  base: '/ls-award/',
   plugins: [
     vue(),
     vueJsx({
