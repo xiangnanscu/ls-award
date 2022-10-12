e7024c203dde2e8ccff8ab9dbb903c797cced7d6
xn <280145668@qq.com>
2022-10-12 19:30:14 +0800

    push

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,16 @@
 // import { ref, reactive, onMounted } from 'vue'
 import { read, utils, writeFileXLSX } from 'xlsx/xlsx.mjs';
 import { version } from "../package.json"
+import XTable from "./components/XTable.vue"
 
 // console.log({ dataFile })
 const records = ref([])
 const resultText = ref("")
+const addressRows = ref([])
 function exportFile() {
   const data = records.value
   const res = {}
+  const addressStat = {}
   for (const row of data) {
     let e = res[row.手机号]
     if (e) {
@@ -20,7 +23,13 @@ function exportFile() {
     } else {
       res[row.手机号] = row
     }
+    let addr = row.答题市
+    if (addressStat[addr] === undefined) {
+      addressStat[addr] = 0
+    }
+    addressStat[addr] += 1
   }
+  addressRows.value = Object.entries(addressStat).sort((a,b)=>b[1]-a[1]).map(([name, cnt])=>({地名:name, 人数:cnt}))
   const uniqueRows = Object.values(res)
   console.log({ uniqueRows })
   uniqueRows.sort((a,b)=>b.分数-a.分数)
@@ -35,12 +44,15 @@ async function readXlsx(event) {
   const wb = read(await f.arrayBuffer());
   const data = utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]]);
   records.value = data.map(e => {
-    let [姓名, 手机号, 身份证, 地址 ] = e.来源详情.split('/')
+    let [姓名, 手机号, 身份证, 地址] = e.来源详情.split('/')
+    let [_,答题市] = e.来自IP.split(/[()]/)
     let res = {
-      姓名, 手机号, 身份证, 地址, IP: e['来自IP'], 分数: e['总分'], 时间: e.提交答卷时间}
+      姓名, 手机号, 身份证, 地址, IP: e['来自IP'], 分数: e['总分'], 时间: e.提交答卷时间,  答题市
+}
     return res
   })
   // console.log(records.value)
+  resultText.value = "处理中。。。"
   exportFile()
 }
 onMounted(() => {
@@ -53,6 +65,7 @@ onMounted(() => {
 <template>
     <div class="bk">
       版本：{{ version }}<input type="file" @input="readXlsx" /> <span>{{resultText }}</span>
+      <x-table v-if="addressRows.length" :rows="addressRows"></x-table>
     </div>
 </template>
 
